5873521d1a695586b4c5534cc2016f24a0cc4230
ShablyginMihail <146061574+ShablyginMihail@users.noreply.github.com>
2024-03-29 16:29:51 +0300

    Add files via upload

--- lab7.cs
+++ lab7.cs
@@ -7,11 +7,11 @@ namespace lab7
         protected string _name;
         protected string _surname;
         protected string _lastname;
-        public Human (string name, string surname, string lastname)
+        public Human (string _name, string sur_name, string last_name)
         {
-            _name = name;
-            _surname = surname;
-            _lastname = lastname;
+            _name = _name;
+            _surname = sur_name;
+            _lastname = last_name;
         }
     }
     class Student: Human
@@ -19,12 +19,13 @@ namespace lab7
         private double _average = 0;
         private int _id;
         private bool _banned = false;
+        private static int nextId = 0;
         public double Average { get { return _average; } }
         public bool Banned { get { return _banned; } }
 
-        public Student(string name, string surname, string lastname, int id, int[] grades) : base(name, surname, lastname)
+        public Student(string _name, string sur_name, string last_name, int[] grades) : base(_name, sur_name, last_name)
         {
-            _id = id;
+            _id = ++nextId;
             for (int i = 0; i < 3; i++)
             {
                 _average += grades[i];
@@ -47,17 +48,17 @@ namespace lab7
     {
         class Sportsman //1
         {
-            private string _name;
-            private string _surname;
+            private string __name;
+            private string _sur_name;
             private double _res1;
             private double _res2;
             private double _best_res;
             private bool _disqualified = false;
             public double BestResult { get { return _best_res; } }
-            public Sportsman(string name, string surname, double res1, double res2)
+            public Sportsman(string _name, string sur_name, double res1, double res2)
             {
-                _name = name;
-                _surname = surname;
+                __name = _name;
+                _sur_name = sur_name;
                 _res1 = res1;
                 _res2 = res2;
                 if (_res1 > _res2) _best_res = _res1;
@@ -66,21 +67,18 @@ namespace lab7
             public void Disqualify() => _disqualified = true;
             public void PrintResult(string text = "Информация некорректна")
             {
-                if (_name != null && _surname != null && _best_res > 0)
+                if (__name != null && _sur_name != null && _best_res > 0)
                 {
-                    text = _surname + " " + _name + "    " + _best_res;
+                    text = _sur_name + " " + __name + "    " + _best_res;
                 }
                 if (!_disqualified) Console.WriteLine(text);
             }
-        } 
+        }
         class Team //3
         {
             protected string _name;
             protected int _totalScore = 0;
             protected int _delta = 0;
-            public string Name { get { return _name; } }
-            public int Score { get { return _totalScore; } set { _totalScore = value; } }
-            public int Delta { get { return _delta; } set { _delta = value; } }
 
             public Team(string name)
             {
@@ -93,7 +91,7 @@ namespace lab7
                 _totalScore += 3;
                 _delta += delta;
             }
-            public  void Lose(int delta)
+            public void Lose(int delta)
             {
                 _delta -= delta;
             }
@@ -101,7 +99,55 @@ namespace lab7
             {
                 _totalScore += 1;
             }
-
+            public static void Match(Team team1, Team team2, int score1, int score2)
+            {
+                Console.WriteLine(team1._name + " - " + team2._name + " " + score1 + ":" + score2);
+                int delta = Math.Abs(score1 - score2);
+                if (score1 > score2)
+                {
+                    team1.Win(delta);
+                    team2.Lose(delta);
+                }
+                else if (score1 < score2)
+                {
+                    team2.Win(delta);
+                    team1.Lose(delta);
+                }
+                else
+                {
+                    team1.Draw();
+                    team2.Draw();
+                }
+            }
+            static int Findpvt(Team[] teams, int minIndex, int maxIndex)
+            {
+                int pvt = minIndex;
+                Team tmp;
+                for (int i = minIndex; i < maxIndex; i++)
+                {
+                    if (teams[i]._totalScore > teams[maxIndex]._totalScore)
+                    {
+                        tmp = teams[pvt];
+                        teams[pvt] = teams[i];
+                        teams[i] = tmp;
+                        pvt++;
+                    }
+                }
+                tmp = teams[pvt];
+                teams[pvt] = teams[maxIndex];
+                teams[maxIndex] = tmp;
+                return pvt;
+            }
+            public static void QuickSort(Team[] teams, int minIndex, int maxIndex)
+            {
+                if (minIndex >= maxIndex)
+                {
+                    return;
+                }
+                int pvt = Findpvt(teams, minIndex, maxIndex);
+                QuickSort(teams, minIndex, pvt - 1);
+                QuickSort(teams, pvt + 1, maxIndex);
+            }
         }
         class ManTeam: Team
         {
@@ -127,39 +173,7 @@ namespace lab7
             }
             return grade;
         }
-        static void Match(ref Team team1, ref Team team2, int score1, int score2)
-        {
-            Console.WriteLine(team1.Name + " - " + team2.Name + " " + score1 + ":" + score2);
-            int delta = Math.Abs(score1 - score2);
-            if (score1 > score2)
-            {
-                team1.Win(delta);
-                team2.Lose(delta);
-            }
-            else if (score1 < score2)
-            {
-                team2.Win(delta);
-                team1.Lose(delta);
-            }
-            else
-            {
-                team1.Draw();
-                team2.Draw();
-            }
-        }
-        static void SortTeams(Team[] teams)
-        {
-            Team tmp;
-            for (int i = 1; i < teams.Length; i++)
-            {
-                for (int j = i; j > 0 && ((teams[j].Score > teams[j - 1].Score) || (teams[j].Score == teams[j - 1].Score && teams[j].Delta > teams[j - 1].Delta)); j--)
-                {
-                    tmp = teams[j];
-                    teams[j] = teams[j - 1];
-                    teams[j - 1] = tmp;
-                }
-            }
-        }
+        
 
         static void Main()
         {
@@ -167,21 +181,21 @@ namespace lab7
             //Console.Write("Введите количество участников: ");
             //int number = int.Parse(Console.ReadLine());
             //Sportsman[] sportsmen = new Sportsman[number];
-            //string name, surname;
+            //string _name, sur_name;
             //double res1, res2;
             //for (int i = 0; i < number; i++)
             //{
             //    Console.WriteLine("Участник " + (i + 1));
             //    Console.Write("Фамилия: ");
-            //    surname = Console.ReadLine();
+            //    sur_name = Console.ReadLine();
             //    Console.Write("Имя: ");
-            //    name = Console.ReadLine();
+            //    _name = Console.ReadLine();
             //    Console.Write("Результат первой попытки: ");
             //    res1 = double.Parse(Console.ReadLine());
             //    Console.Write("Результат второй попытки: ");
             //    res2 = double.Parse(Console.ReadLine());
             //    Console.WriteLine();
-            //    sportsmen[i] = new Sportsman(name, surname, res1, res2);
+            //    sportsmen[i] = new Sportsman(_name, sur_name, res1, res2);
             //}
 
             //Sportsman tmp;
@@ -206,21 +220,18 @@ namespace lab7
             //Console.Write("Введите количество учеников: ");
             //int number = int.Parse(Console.ReadLine());
             //Student[] students = new Student[number];
-            //string name, surname, lastname;
-            //int id;
+            //string _name, sur_name, last_name;
             //int[] grades = new int[3];
             //Console.WriteLine();
             //for (int i = 0; i < number; i++)
             //{
             //    Console.WriteLine(i + 1);
             //    Console.Write("Фамилия: ");
-            //    surname = Console.ReadLine();
+            //    sur_name = Console.ReadLine();
             //    Console.Write("Имя: ");
-            //    name = Console.ReadLine();
+            //    _name = Console.ReadLine();
             //    Console.Write("Отчество: ");
-            //    lastname = Console.ReadLine();
-            //    Console.Write("ID: ");
-            //    id = int.Parse(Console.ReadLine());
+            //    last_name = Console.ReadLine();
             //    Console.Write("Оценка по математике: ");
             //    grades[0] = CheckGrade(int.Parse(Console.ReadLine()));
             //    Console.Write("Оценка по физике: ");
@@ -228,7 +239,7 @@ namespace lab7
             //    Console.Write("Оценка по русскому языку: ");
             //    grades[2] = CheckGrade(int.Parse(Console.ReadLine()));
             //    Console.WriteLine();
-            //    students[i] = new Student(name, surname, lastname, id, grades);
+            //    students[i] = new Student(_name, sur_name, last_name, grades);
             //}
 
             //Student tmp;
@@ -265,8 +276,7 @@ namespace lab7
                 {
                     score1 = random.Next(0, 6);
                     score2 = random.Next(0, 6);
-
-                    Match(ref man_teams[i], ref man_teams[j], score1, score2);
+                    Team.Match(man_teams[i], man_teams[j], score1, score2);
                 }
             }
             Console.WriteLine();
@@ -281,7 +291,7 @@ namespace lab7
                     score1 = random.Next(0, 6);
                     score2 = random.Next(0, 6);
 
-                    Match(ref woman_teams[i], ref woman_teams[j], score1, score2);
+                    Team.Match(woman_teams[i], woman_teams[j], score1, score2);
                 }
             }
             for (int i = 0; i < 4; i++)
@@ -292,7 +302,7 @@ namespace lab7
             {
                 teams[i + 4] = woman_teams[i];
             }
-            SortTeams(teams);
+            Team.QuickSort(teams, 0, teams.Length - 1);
             Console.WriteLine();
             for (int i = 0; i < 8; i++)
             {
