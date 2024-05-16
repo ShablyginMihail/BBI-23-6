eab90940ee34acb90a2c87cb44edc04f78208cd3
ShablyginMihail <146061574+ShablyginMihail@users.noreply.github.com>
2024-05-16 17:28:15 +0300

    9️⃣.1

---
+++ Lab9_1/Lab9_1/MySerializeXML.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lab9_1.Serializers
+{
+	internal class MySerializeXML : ISer
+	{
+		public MySerializeXML(){ }
+
+        public void Write<T>(T obj, string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                new XmlSerializer(typeof(T)).Serialize(fs, obj);
+            }
+        }
+
+        public T Read<T>(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                return (T)new XmlSerializer(typeof(T)).Deserialize(fs);
+            }
+        }
+    }
+}
+
